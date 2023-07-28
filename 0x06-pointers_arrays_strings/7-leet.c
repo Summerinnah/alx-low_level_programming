d38f7c18308c632a8d815827a803ab83089c399c
Summerinnah <ziyandaqwatekana@gmail.com>
2023-07-28 13:02:15 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,6 +2,26 @@
 
 /**
  * leet - encodes a string into 1337
- * @s: input string
- * Return: s value
- *
+ * @n: input string
+ * Return: pointer to the destination
+ */
+
+char *leet(char *n)
+{
+	int y;
+	int z;
+	char str1[] = "aAeEoOtTlL";
+	char str2[] = "4433007711";
+
+	for (y = 0; n[y] != '\0'; y++)
+	{
+		for (z = 0; z < 10; z++)
+		{
+			if (n[y] == str1[z])
+			{
+				n[y] = str2[z];
+			}
+		}
+	}
+	return (n);
+}
