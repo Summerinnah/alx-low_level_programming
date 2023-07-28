712663904f4d4b8c5fd86d19e4934f4c8aefeecf
Summerinnah <ziyandaqwatekana@gmail.com>
2023-07-28 12:22:54 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
- * cap_string - function that capitalizes all words of a string
- * @s: input string
- * Return: pointer to the altered string
+ * cap_string - fuction that capitalizes all words of a string
+ * @str: string to be capitalized
+ * Return: pointer to the destinatio
  */
 
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
 	int digit = 0;
 
-	while (s[digit])
+	while (str[digit])
 	{
-		while (!(s[digit] >= 'a' && s[digit] <= 'z'))
+		while (!(str[digit] >= 'a' && str[digit] <= 'z'))
 			digit++;
 
-		if (s[digit - 1] == '' ||
-		    s[digit - 1] == '\t' ||
-		    s[digit - 1] == '\n' ||
-		    s[digit - 1] == ',' ||
-		    s[digit - 1] == ';' ||
-		    s[digit - 1] == '.' ||
-		    s[digit - 1] == '!' ||
-		    s[digit - 1] == '?' ||
-		    s[digit - 1] == '"' ||
-		    s[digit - 1] == '(' ||
-		    s[digit - 1] == ')' ||
-		    s[digit - 1] == '{' ||
-		    s[digit - 1] == '}' ||
+		if (str[digit - 1] == ' ' ||
+		    str[digit - 1] == '\t' ||
+		    str[digit - 1] == '\n' ||
+		    str[digit - 1] == ',' ||
+		    str[digit - 1] == ';' ||
+		    str[digit - 1] == '.' ||
+		    str[digit - 1] == '!' ||
+		    str[digit - 1] == '?' ||
+		    str[digit - 1] == '"' ||
+		    str[digit - 1] == '(' ||
+		    str[digit - 1] == ')' ||
+		    str[digit - 1] == '{' ||
+		    str[digit - 1] == '}' ||
 		    digit == 0)
-			s[digit] -= 32;
+			str[digit] -= 32;
 
 		digit++;
 	}
 
-	return (s);
+	return (str);
 }
