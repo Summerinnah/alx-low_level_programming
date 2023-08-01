98e0bfab724ad12b7e0b6c29e7067273c1dd20d4
Summerinnah <ziyandaqwatekana@gmail.com>
2023-08-01 07:44:25 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,11 +11,9 @@ void print_chessboard(char (*a)[8])
 	int c;
 	int k;
 
-	c = 0;
-	for (c < 8; c++)
+	for (c = 0; c < 8; c++)
 	{
-		k = 0;
-		for (k < 8; k++)
+		for (k = 0; k < 8; k++)
 			_putchar(a[c][k]);
 		_putchar('\n');
 	}
